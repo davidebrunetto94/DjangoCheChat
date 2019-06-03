8ab57d402f4fc7e43201b222a510d482375095d6
Sinhuè Angelo Rossi <sinhue.rossi@gmail.com>
2019-06-03 19:19:30 +0200

    Aggiunte classi su models.py

--- CheChatApp/models.py
+++ CheChatApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class User(models.Model):
     """User model"""
     username = models.CharField('Username', max_length=20)
@@ -9,4 +8,21 @@ class User(models.Model):
     email = models.EmailField('Email')
     profileImage = models.URLField('Profile image URL')
     lastAccess = models.DateTimeField('Last access')
+
+
+class Chat(models.Model):
+    title = models.CharField('Title', max_length=30)
+    # se non è un gruppo, title = None
+    participants = models.ManyToManyField(User)
+
+class PhoneBook(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    contacts = models.ManyToManyField(User)
+
+class Message(models.Model):
+    text = models.CharField('Text', max_length=5000)
+    timestamp = models.DateTimeField('Timestamp')
+    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+
 
