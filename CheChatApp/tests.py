df582be4df21de88737cd2dfcc3f3060caa0c412
Davide <davide.brunetto12@gmail.com>
2019-06-04 16:21:13 +0200

    Prime prove test

--- CheChatApp/tests.py
+++ CheChatApp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
-
+import requests
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from CheChatApp.views import login
+from CheChatApp.views import logout
+from django.shortcuts import render
 # Create your tests here.
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_login(self):
+        user = User.objects.create_user(username='davide', email='davide.brunetto12@gmail.com',password='ciao12345')
+        data = {
+            "username": "davide",
+            "password": "ciao12345",
+        }
+
+        URL = 'http://127.0.0.1:8000/'
+        request = self.client.post(URL, data=data)
+        self.assertContains(request, 'Chat')
